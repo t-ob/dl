8b1b025f553d42f13daea0102d7678b154acdefa
Tom O'Brien <tom@lumi.news>
2017-01-02 13:26:55 +0000

    Add first linalg steps

---
+++ src/linalg.cpp
@@ -0,0 +1,151 @@
+#include <iostream>
+#include <stdexcept>
+#include <vector>
+#include "linalg.h"
+
+Matrix::Matrix(int r, int c) : _rows(r), _cols(c)
+{
+    _data.resize(r * c, 0.0);
+}
+
+void Matrix::set(int i, int j, double x)
+{
+    _data[_cols * i + j] = x;
+}
+
+double Matrix::get(int i, int j) const
+{
+    return _data[_cols * i + j];
+}
+
+int Matrix::rows() const
+{
+    return _rows;
+}
+
+int Matrix::cols() const
+{
+    return _cols;
+}
+
+Matrix Matrix::transpose() const
+{
+    Matrix mt(_cols, _rows);
+    for (int i = 0; i < _rows; ++i)
+    {
+        for (int j = 0; j < _cols; ++j)
+        {
+            mt.set(j, i, this->get(i, j));
+        }
+    }
+
+    return mt;
+}
+
+Matrix identity_matrix(int n)
+{
+    Matrix m(n, n);
+    for (int i = 0; i < n; ++i)
+        m.set(i, i, 1.0);
+
+    return m;
+}
+
+void repr(Matrix m)
+{
+    for (int i = 0; i < m.rows(); ++i)
+    {
+        for (int j = 0; j < m.cols(); ++j)
+        {
+            std::cout << m.get(i, j) << " ";
+        }
+        std::cout << std::endl;
+    }
+}
+
+Matrix operator+(const Matrix &a, const Matrix &b)
+{
+    // Matrices be of same shape
+    if (a.rows() != b.rows() || a.cols() != b.cols())
+        throw std::domain_error("Matrices wrong shape");
+
+    Matrix c(a.rows(), a.cols());
+    for (int i = 0; i < a.rows(); ++i)
+    {
+        for (int j = 0; j < a.cols(); ++j)
+        {
+            c.set(i, j, a.get(i, j) + b.get(i, j));
+        }
+    }
+
+    return c;
+}
+
+Matrix operator*(const Matrix &a, const Matrix &b)
+{
+    // Number of columns in LHS must be equal to rows in RHS
+    if (a.cols() != b.rows())
+        throw std::domain_error("Incompatible shape");
+
+    Matrix c(a.rows(), b.cols());
+    for (int i = 0; i < a.rows(); ++i)
+    {
+        for (int j = 0; j < b.cols(); ++j)
+        {
+            double c_ij = 0.0;
+            for (int k = 0; k < a.cols(); ++k)
+            {
+                c_ij += a.get(i, k) * b.get(k, j);
+            }
+            c.set(i, j, c_ij);
+        }
+    }
+
+    return c;
+}
+
+double fib(int n)
+{
+    Matrix f(2, 2);
+    f.set(0, 0, 0.0);
+    f.set(0, 1, 1.0);
+    f.set(1, 0, 1.0);
+    f.set(1, 1, 1.0);
+
+    Matrix b(2, 1);
+    b.set(0, 0, 0.0);
+    b.set(1, 0, 1.0);
+
+    while (n > 0) {
+        b = f * b;
+        n--;
+    }
+
+    return b.get(0, 0);
+}
+
+int main()
+{
+    Matrix m = identity_matrix(3);
+
+    repr(m);
+
+    Matrix a(2, 2);
+    a.set(0, 0, 1.0);
+    a.set(0, 1, 1.0);
+    a.set(1, 0, 0.0);
+    a.set(1, 1, 1.0);
+
+    repr(a);
+    repr(a.transpose());
+
+    repr(a + a.transpose());
+
+    repr(a * a);
+    repr(a * a * a);
+
+    for (int i = 0; i <= 10; ++i)
+        std::cout << i << "\t" << fib(i) << std::endl;
+
+    return 0;
+}
